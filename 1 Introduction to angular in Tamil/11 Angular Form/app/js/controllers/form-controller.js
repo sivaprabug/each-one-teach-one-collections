485db9897c07042934b69eb9971379f75d57fb92
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-02-01 22:44:30 +0530

    ng-init concepts

--- 1 Introduction to angular in Tamil/11 Angular Form/app/js/controllers/form-controller.js
+++ 1 Introduction to angular in Tamil/11 Angular Form/app/js/controllers/form-controller.js
@@ -1,6 +1,4 @@
 app.controller('formController', ['$scope', function($scope) {
-    $scope.user = [];
-    $scope.user.userName = "Sivaprabu Ganesan";
     $scope.onSubmit = function() {
         console.log($scope.user);
     }
